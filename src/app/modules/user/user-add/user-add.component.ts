792f333d01667f72af56eabf270a8b21a27fff2a
davidgalvez <davidgalvezfis@gmail.com>
2023-08-02 20:01:36 +0200

    implementing user-registration form validation

--- src/app/modules/user/user-add/user-add.component.ts
+++ src/app/modules/user/user-add/user-add.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Toaster } from 'ngx-toast-notifications';
 
 @Component({
   selector: 'app-user-add',
@@ -9,4 +11,19 @@ export class UserAddComponent {
   name : any = null
   email : any = null                    
   password : any = null
+  constructor(
+    public Toaster:Toaster,
+    public modal:NgbActiveModal,
+  ){}
+
+  store(){
+    if(!this.name||!this.email||!this.password){
+      this.Toaster.open({text:"All fields are required", caption:'VALIDATION', type:"danger"});
+      return
+    }
+    let formData = new FormData();
+    formData.append("name",this.name)
+    formData.append("email",this.email)
+    formData.append("password",this.password)
+  }
 }
